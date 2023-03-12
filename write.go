7916f94d6b8bd870357780e321896bb7a87faa6f
Bakarseck <seck.bakar@ugb.edu.sn>
2023-03-12 06:59:35 -0700

    Deal with file

--- write.go
+++ write.go
@@ -3,10 +3,13 @@ package file
 import (
 	"os"
 	"strings"
+	"math"
 )
 
 
 
+
+/*****************Read********************/
 func Read(filename string, sep string) ([]string, error) {
 
 	sampleFile, err := os.ReadFile(filename)
@@ -16,12 +19,16 @@ func Read(filename string, sep string) ([]string, error) {
 
 }
 
+/*****************Check********************/
 func Check(err error) {
 	if err != nil {
 		panic(err)
 	}
 }
 
+
+
+/*****************Write********************/
 func Write(filename string, content []string, sep string) error {
 	var tabByte []byte
 	for index, v := range content {
@@ -32,4 +39,59 @@ func Write(filename string, content []string, sep string) error {
 		}
 	}
 	return os.WriteFile(filename, tabByte, 0777)
+}
+
+
+/*****************Average********************/
+func Average(tabFloat []float64) float64 {
+	sum := 0.0
+	for _, v := range tabFloat {
+		sum += v
+	}
+	return sum / float64(len(tabFloat))
+}
+
+/*****************Median********************/
+func Median(tabFloat []float64) int {
+	sortFloat64(tabFloat)
+	length := len(tabFloat)
+	middle := len(tabFloat) / 2
+	median := 0.0
+	if length % 2 == 0 {
+		middleLeft := tabFloat[middle -1]
+		middleRight := tabFloat[middle]
+		median = (middleLeft + middleRight) / 2
+	} else if length % 2 != 0{
+		median = tabFloat[middle]
+	}
+	return int(math.Round(median))
+}
+
+
+/*****************Variance********************/
+func Variance(tabFloat []float64, mean float64) int {
+	sum := 0.0
+	for _, v := range tabFloat {
+		sum += math.Pow(v - mean, 2)
+	}
+	return int(math.Round(sum / float64(len(tabFloat))))
+}
+
+/*****************Standard Deviation********************/
+func StandardDeviation(tabFloat []float64, mean float64) int {
+ 	return int(math.Round( math.Sqrt(float64(Variance(tabFloat, mean)))))
+}
+
+
+/*****************SortFloat64********************/
+func sortFloat64(numbers []float64) {
+	for i := 0; i < len(numbers); i++ {
+		for j := i + 1; j < len(numbers); j++ {
+			if numbers[j] < numbers[i] {
+				temp := numbers[i]
+				numbers[i] = numbers[j]
+				numbers[j] = temp
+			}
+		}
+	}
 }
--- write_test.go
+++ write_test.go
@@ -26,3 +26,37 @@ func TestRead(t *testing.T)  {
 	}
 
 }
+
+func TestAverage(t *testing.T) {
+
+	result := Average([]float64{1.0, 2.0, 3.0})
+
+	if result != 2 {
+		t.Errorf("Error !!!")
+	}
+}
+
+func TestVariance(t *testing.T) {
+
+	result := Variance([]float64{1.0, 2.0, 3.0}, Average([]float64{1.0, 2.0, 3.0}))
+
+	if result != 1 {
+		t.Errorf("Error !!!")
+	}
+}
+
+func TestStandardDeviation(t *testing.T) {
+	result := StandardDeviation([]float64{1.0, 2.0, 3.0}, Average([]float64{1.0, 2.0, 3.0}))
+
+	if result != 1 {
+		t.Errorf("Error !!!")
+	}
+}
+
+func TestMedian(t *testing.T) {
+	result := Median([]float64{1.0, 2.0, 3.0})
+
+	if result != 2 {
+		t.Errorf("Error !!!")
+	}
+}
